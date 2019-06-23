3c4dc004982ea939101663c4cd80b0a61dcfa670
GooRiOn <foreverframenet@gmail.com>
2019-06-23 15:19:06 +0200

    Added Rejected events in subscription

--- src/Pacco.Services.Vehicles.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Vehicles.Infrastructure/Extensions.cs
@@ -1,13 +1,17 @@
 using System;
 using Convey;
+using Convey.CQRS.Commands;
 using Convey.CQRS.Queries;
+using Convey.MessageBrokers;
 using Convey.MessageBrokers.CQRS;
 using Convey.MessageBrokers.RabbitMQ;
 using Convey.Persistence.MongoDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Pacco.Services.Vehicles.Application.Commands;
+using Pacco.Services.Vehicles.Application.Events;
 using Pacco.Services.Vehicles.Application.Messaging;
+using Pacco.Services.Vehicles.Core.Entities;
 using Pacco.Services.Vehicles.Core.Repositories;
 using Pacco.Services.Vehicles.Infrastructure.IoC;
 using Pacco.Services.Vehicles.Infrastructure.Messaging;
@@ -35,9 +39,13 @@ namespace Pacco.Services.Vehicles.Infrastructure
         public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
         {
             app.UseRabbitMq()
-                .SubscribeCommand<AddVehicle>()
-                .SubscribeCommand<UpdateVehicle>()
-                .SubscribeCommand<DeleteVehicle>();
+                .SubscribeCommand<AddVehicle>((cmd, ex) => new AddVehicleRejected(cmd.Id, ex.Message, ex.Code))
+                .SubscribeCommand<UpdateVehicle>((cmd, ex) => new UpdateVehicleRejected(cmd.Id, ex.Message, ex.Code))
+                .SubscribeCommand<DeleteVehicle>((cmd, ex) => new DeletedVehicleRejected(cmd.Id, ex.Message, ex.Code));
+
+            var handler = app.ApplicationServices.GetService<IBusPublisher>();
+
+            handler.SendAsync(new AddVehicle(Guid.NewGuid(), "", "", "", 12, 12, Variants.Animal), CorrelationContext.Empty);
             
             return app;
         }
